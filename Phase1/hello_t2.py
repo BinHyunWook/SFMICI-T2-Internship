4c8028a8124f6c4fae01b49d06de845ffb29019b
cloud.park <vividxenon@gmail.com>
2022-04-13 14:39:15 +0900

    hello_t2.py가 Shell에서 ip 가져오도록 수정

--- Phase1/hello_t2.py
+++ Phase1/hello_t2.py
@@ -1,11 +1,17 @@
-
 from flask import Flask, request
+import subprocess
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    host = request.host
-    return 'Hello SFMICI-T2 Internship  ' +'serviced from  ' +  host
+    # Since Flask is WSGI Engine, Hosted Server's IP address will get by shell script
+    hostip = subprocess.check_output(
+        "hostname -I", shell=True
+    ).decode('utf-8')
+    
+
+    return 'Hello SFMICI-T2 Internship  ' + 'serviced from  >>>>      ' +  hostip
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
